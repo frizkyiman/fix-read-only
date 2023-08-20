127c670ecba65ef7b8835ca4d16e7718354be2d7
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-08-20 23:00:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_roh && chmod +x /etc/init.d/repair_ro
-wget --no-check-certificate https://raw.githubusercontent.comfrizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro && chmod +x /usr/bin/repair_ro
+wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro && chmod +x /usr/bin/repair_ro
 rm /tmp/install.sh
