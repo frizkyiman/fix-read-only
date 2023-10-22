de1dab7a40dfc3af5b1e9214f85e653ee62a2054
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-10-22 21:53:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_ro && chmod +x /etc/init.d/repair_ro
-wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro && chmod +x /usr/bin/repair_ro
-rm /tmp/install.sh
+if [ -e /etc/init.d/repair_ro ]; then
+    rm /etc/init.d/repair_ro
+else
+    wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_ro && chmod +x /etc/init.d/repair_ro
+    wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro && chmod +x /usr/bin/repair_ro
+    sed -i '/exit 0/i /etc/init.d/repair_ro' /etc/rc.local
+    rm /tmp/install.sh
+fi
