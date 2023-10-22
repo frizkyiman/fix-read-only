14d563aac85796ee2aabe132c9ee698fe68fbe9b
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-10-22 22:39:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ if [ -e /etc/init.d/repair_ro ]; then
     rm /etc/init.d/repair_ro
 fi
 echo "Installing..."
-wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_ro  > /dev/null
-wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro  > /dev/null
+wget --quiet --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_ro  > /dev/null  2>&1
+wget --quiet --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro  > /dev/null  2>&1
 
 echo "Setting permission..."
 chmod +x /etc/init.d/repair_ro  > /dev/null
@@ -13,7 +13,7 @@ chmod +x /usr/bin/repair_ro  > /dev/null
 echo "Adding to startup..."
 sed -i '/exit 0/i /etc/init.d/repair_ro' /etc/rc.local  > /dev/null
 
-echo "Cleaning'''"
+echo "Cleaning..."
 rm /tmp/install.sh  > /dev/null
 
 echo "Success!"
