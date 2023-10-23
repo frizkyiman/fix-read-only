f1aa55438ad182b0ca833f92901c3e5afb943004
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-10-23 11:35:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-if [ -e /etc/init.d/repair_ro ]; then
-    rm /etc/init.d/repair_ro
+if [ -e /usr/bin/repair_ro ]; then
+    rm /usr/bin/repair_ro
 fi
 echo "Installing..."
-wget --quiet --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /etc/init.d/repair_ro  > /dev/null  2>&1
 wget --quiet --no-check-certificate https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro -O /usr/bin/repair_ro  > /dev/null  2>&1
 
 echo "Setting permission..."
-chmod +x /etc/init.d/repair_ro  > /dev/null
 chmod +x /usr/bin/repair_ro  > /dev/null
 
 echo "Adding to startup..."
-sed -i '/exit 0/i /etc/init.d/repair_ro' /etc/rc.local  > /dev/null
+if ! grep -q '/usr/bin/repair_ro' /etc/rc.local; then
+    sed -i '/exit 0/i /usr/bin/repair_ro' /etc/rc.local 2>/dev/null
+fi
 
 echo "Cleaning..."
 rm /tmp/install.sh  > /dev/null
