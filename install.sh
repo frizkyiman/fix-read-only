d2470f3c58084e82095cb6a7725c25574ce5671c
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-05 18:12:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
-if [ -e /usr/bin/repair_ro ]; then
+if [ -e /sbin/repair_ro -a -e /etc/init.d/repair_ro -a -e /usr/bin/repair_ro ]; then
+    rm /sbin/repair_ro
     rm /usr/bin/repair_ro
+    rm /etc/init.d/repair_ro
 fi
+
 echo "Installing..."
 wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro -O /sbin/repair_ro  > /dev/null  2>&1
-wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O /usr/bin/repair_ro  > /dev/null  2>&1
+
+cat <<'EOF' >/etc/init.d/repair_ro
+#!/bin/sh /etc/rc.common
+
+START=99
+
+start() {
+    /sbin/repair_ro
+}
+EOF
+
+cat <<'EOF' >/usr/bin/repair_ro
+#!/bin/sh /etc/rc.common
+root_device="$1"
+bash /sbin/repair_ro "$1"
+EOF
 
 echo "Setting permission..."
 chmod +x /sbin/repair_ro  > /dev/null
 chmod +x /etc/init.d/repair_ro  > /dev/null
+chmod +x /usr/bin/repair_ro  > /dev/null
 
 echo "Adding to startup..."
 etc/init.d/repair_ro enable
